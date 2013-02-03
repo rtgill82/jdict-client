b46d95393fbb14b48f8418b91c6524f5296feeab
Robert Gill <rtgill82@gmail.com>
2013-02-02 19:09:02 -0800

    Flesh out test application a little more

--- src/main/java/org/lonestar/sdf/locke/libs/dict/Dict.java
+++ src/main/java/org/lonestar/sdf/locke/libs/dict/Dict.java
@@ -1,47 +1,149 @@
 /**
  * Created:  Fri 21 Dec 2012 11:03:29 PM PST
- * Modified: Sun 27 Jan 2013 02:37:35 PM PST
+ * Modified: Sat 02 Feb 2013 07:00:06 PM PST
  *
  */
 
 package org.lonestar.sdf.locke.libs.dict;
 
 import java.io.IOException;
-import java.util.List;
+import java.util.ArrayList;
+import java.util.Hashtable;
 import java.util.Iterator;
+import java.util.List;
+
 import org.lonestar.sdf.locke.libs.dict.Definition;
 import org.lonestar.sdf.locke.libs.dict.JDictClient;
 import org.lonestar.sdf.locke.libs.dict.DictException;
 import org.lonestar.sdf.locke.libs.dict.Dictionary;
 
 public class Dict {
+    private static void showHelp()
+    {
+        System.out.println("Usage: Dict [options] [word]\n");
+        System.out.println("\t-host <server>\t\t\tDICT server [default: test.dict.org]");
+        System.out.println("\t-port <port>\t\t\tserver port [default: 2628]");
+        System.out.println("\t-serverinfo\t\t\tshow server information");
+        System.out.println("\t-help\t\t\t\tshow server help");
+        System.out.println("\t-dictionaries\t\t\tlist dictionaries available");
+        System.out.println("\t-dictionaryinfo <dictionary>\tget information for <dictionary>");
+    }
+
     public static void main(String[] args)
     {
         JDictClient dictClient;
         List<Definition> definitions;
+        List<Dictionary> dictionaries;
         Definition definition;
+        String info;
+        Hashtable opts;
 
-        if (args.length > 0) {
-            String word = args[0];
+        if (args.length == 0) {
+            showHelp();
+            return;
+        }
 
-            try {
-                dictClient = JDictClient.connect("test.dict.org");
-                definitions = dictClient.define(word);
+        opts = parseOptions(args);
+
+        try {
+            dictClient = JDictClient.connect(
+                    (String) opts.get("host"),
+                    (Integer) opts.get("port")
+                );
+
+            if (opts.containsKey("help")) {
+                System.out.println(dictClient.getHelp());
+                dictClient.close();
+            } else if (opts.containsKey("serverinfo")) {
+                System.out.println(dictClient.getServerInfo());
+                dictClient.close();
+            } else if (opts.containsKey("dictionaryinfo")) {
+                String dictionary;
+                dictionary = (String)opts.get("dictionaryinfo");
+                System.out.println(dictClient.getDictionaryInfo(dictionary));
+                dictClient.close();
+            } else if (opts.containsKey("dictionaries")) {
+                dictionaries = dictClient.getDictionaries();
                 dictClient.close();
 
-                if (definitions != null) {
-                    Iterator itr = definitions.iterator();
+                if (dictionaries != null) {
+                    Iterator itr = dictionaries.iterator();
                     while (itr.hasNext()) {
-                        definition = (Definition) itr.next();
-                        System.out.println(definition);
+                        Dictionary dictionary = (Dictionary)itr.next();
+                        System.out.println(dictionary);
                     }
                 } else {
-                    System.out.println("No definitions found.");
+                    System.out.println("No dictionaries found.");
                 }
-
-            } catch (IOException e) {
-                e.printStackTrace();
+            } else if (opts.containsKey("words")) {
+                Iterator words = ((ArrayList)opts.get("words")).iterator();
+                while(words.hasNext()) {
+                    String word = (String)words.next();
+                    definitions = dictClient.define(word);
+                    if (definitions != null) {
+                        Iterator defs = definitions.iterator();
+                        while (defs.hasNext()) {
+                            definition = (Definition) defs.next();
+                            System.out.println(definition);
+                        }
+                    } else {
+                        System.out.println(word + ": No definitions found.");
+                    }
+                }
+                dictClient.close();
+            } else {
+                showHelp();
+                return;
             }
+
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+    }
+
+    private static Hashtable parseOptions(String[] args)
+    {
+        String optname = null;
+        Hashtable opts = new Hashtable();
+        ArrayList<String> words = new ArrayList<String>();
+
+        opts.put("host", "test.dict.org");
+        opts.put("port", 2628);
+
+        try {
+            for (int i = 0; i < args.length; i++) {
+                if (args[i].charAt(0) == '-') {
+                    optname = args[i].substring(1);
+
+                    /* String options */
+                    if (optname.equals("host")
+                            || optname.equals("dictionaryinfo")) {
+                        opts.put(optname, args[++i]);
+
+                        /* Integer options */
+                    } else if (optname.equals("port")) {
+                        opts.put(optname, Integer.parseInt(args[++i]));
+
+                        /* Flag options */
+                    } else if (optname.equals("serverinfo")
+                            || optname.equals("help")
+                            || optname.equals("dictionaries")) {
+                        opts.put(optname, true);
+                            }
+                } else {
+                    words.add(args[i]);
+                    opts.put("words", args[i]);
+                }
+            }
+
+            if (words.size() > 0) {
+                opts.put("words", words);
+            }
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Error: " + optname + " expects an argument.");
+            System.exit(1);
+        }
+
+        return opts;
     }
 }

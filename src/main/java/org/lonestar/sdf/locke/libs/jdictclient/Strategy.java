f165dd356e5336c6ebd8f184bc25f58e239207f4
Robert Gill <rtgill82@gmail.com>
2018-05-12 13:53:27 -0700

    Rename project name to jdict-client

--- src/main/java/org/lonestar/sdf/locke/libs/jdictclient/Strategy.java
+++ src/main/java/org/lonestar/sdf/locke/libs/jdictclient/Strategy.java
@@ -1,20 +1,20 @@
 /*
  * Copyright (C) 2016 Robert Gill <locke@sdf.lonestar.org>
  *
- * This file is part of JDictClient.
+ * This file is part of jdict-client.
  *
- * JDictClient is free software: you can redistribute it and/or modify
+ * jdict-client is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
  *
- * JDictClient is distributed in the hope that it will be useful, but
+ * jdict-client is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
- * License along with JDictClient.
+ * License along with jdict-client.
  * If not, see <http://www.gnu.org/licenses/>.
  *
  */
